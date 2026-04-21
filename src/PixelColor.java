fe2d8c28eb422107a7f11ec4a8327b8cda37d4fe
cosmo030 <eliasbraun60@gmail.com>
2026-04-21 16:35:22 +0200

    bildverarbeitungsdateien

---
+++ src/PixelColor.java
@@ -0,0 +1,13 @@
+package vorlage.bild;
+
+public class PixelColor {
+	public int r;
+	public int b;
+	public int g;
+	
+	public PixelColor(int r, int g, int b) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+	}
+}
